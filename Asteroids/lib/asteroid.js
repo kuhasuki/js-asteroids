07f70e9187441eee3494656f450b514872d13520
App Academy Student <student@appacademy.io>
2015-11-30 17:58:35 -0800

    Implement coloscheme v 0.1

--- Asteroids/lib/asteroid.js
+++ Asteroids/lib/asteroid.js
@@ -4,11 +4,12 @@
   if (typeof Asteroids.Asteroid === "undefined") {
     window.Asteroids.Asteroid = {};
   }
-  var DEFCOLOR = "#00CCFF"
+  var items = ["#00CCFF", "#00f058", "#0018f0", "#f0f000", "#f00000"]
+  var DEFCOLOR = items[Math.floor(Math.random()*items.length)];
   var DEFRADIUS = 50;
 
   var Asteroid = Asteroids.Asteroid = function (pos, game) {
-    Asteroids.MovingObject.call(this, pos, Asteroids.Util.randomVec(), DEFRADIUS,DEFCOLOR, game );
+    Asteroids.MovingObject.call(this, pos, Asteroids.Util.randomVec(), DEFRADIUS,items[Math.floor(Math.random()*items.length)], game );
   }
 
   Asteroids.Util.inherits(Asteroid, Asteroids.MovingObject);
